7a2d90dcdba5d2c252fa8c5e34ac67728da53335
裕斗 近藤 <hilotono@icloud.com>
2025-10-10 16:28:56 +0900

    a

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -3,44 +3,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; }
-    // リギドボディ2D
-    public Rigidbody2D rb;
-    // 移動用変数
-    private Vector2 movement;  
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void FixedUpdate()
-    {
-        if (GameManager.Instance != null && GameManager.Instance.isPause == false)
-        {
-            MovePlayer();
-            movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f; // ← 移動スピード設定
 
-        }
-    }
+    private Rigidbody2D rb;
+    private Vector2 movement;
 
-    private void MovePlayer()
-    {
-        rb.MovePosition(rb.position + movement * Status.Instance.PlayerSpeed * Time.deltaTime);
-    }
     void Awake()
     {
         Instance = this;
     }
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 入力取得（WASD / 方向キー）
+        movement = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
     void OnDestroy()
     {
         if (Instance == this)
-        {
             Instance = null;
-        }
     }
 }
