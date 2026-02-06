c22f77a3ce3916ac29df5431a37c57a22970d851
裕斗 近藤 <hilotono@icloud.com>
2026-02-06 16:20:40 +0900

    通常敵修正

--- Assets/Enemy/EnemySpawn.cs
+++ Assets/Enemy/EnemySpawn.cs
@@ -48,7 +48,7 @@ public class EnemySpawn : MonoBehaviour
                     // EnemySpawnオブジェクトの位置を基準にした相対位置でスポーン
                     Vector3 spawnPos = transform.position + (Vector3)p;
                     GameObject enemyObj = Instantiate(data.enemyPrefab, spawnPos, data.rot);
-                    enemyObj.tag = "BossEnemy";
+                    enemyObj.tag = "Boss";
                     spawnedEnemies.Add(enemyObj);
                     enemyObj.SetActive(true);
                     // プレイヤーのTransformを取得
--- Assets/Enemy/MagicBullet.cs
+++ Assets/Enemy/MagicBullet.cs
@@ -2,34 +2,20 @@ using UnityEngine;
 
 public class MagicBullet : MonoBehaviour
 {
+    public bool IsReflected { get; private set; }
     public ReflectMage shooter;
-    public float speed = 10f;
 
     Rigidbody2D rb;
-
-    public bool IsReflected { get; private set; } = false;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Reflect(Vector2 reflectDir)
+    // ”½Ëˆ—
+    public void Reflect(Vector2 newDirection)
     {
-        if (IsReflected) return;
-
-        IsReflected = true;
-
-        rb.linearVelocity = reflectDir.normalized * speed;
-
-        int layer = LayerMask.NameToLayer("PlayerBullet");
-        if (layer != -1)
-            gameObject.layer = layer;
-        else
-            Debug.LogWarning("PlayerBullet layer not found!");
-
-        gameObject.tag = "PlayerBullet";
-        GetComponent<SpriteRenderer>().color = Color.cyan;
+        IsReflected = !IsReflected; // ”½Ëó‘Ô‚ğƒgƒOƒ‹
+        rb.linearVelocity = newDirection.normalized * rb.linearVelocity.magnitude;
     }
-
 }
